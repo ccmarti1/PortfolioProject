2808df320048bbbbe13e8821839ef1902df32105
ccmarti1 <quantomjack@gmail.com>
2021-11-22 14:41:40 -0600

    11/22/2021
    Updated version of portfolio page for Final exam

---
+++ PortfolioDevelopment/Calulator/Calculator.js
@@ -0,0 +1,46 @@
+function calculate(choice)
+{
+    // creates varieable n1
+    // locates num1 id using document.getElementById
+    // .value is added to obtain its value
+    let n1 = parseFloat(document.getElementById('num1').value);
+    let n2 = parseFloat(document.getElementById('num2').value);
+    let result;
+    // holds value of choice
+    let ch= choice;
+// switch takes one argument
+    switch(ch)
+    {
+        // If 1 then run this operation
+        case '1':
+            result = n1+n2;
+            break;
+        // if 2 is sellected run this operation
+        case '2':
+            result = n1-n2;
+            break;
+        // If 3 is entered run this preogram
+        case '3':
+            result= n1*n2;
+            break;
+        // If 4 is clicked run this program
+        case '4':
+            result=n1/n2;
+            break;
+        case '5':
+            result=n1%n2;
+            break;
+        case 'num1':
+            result=n1%n2;
+            break;
+        case 'num2':
+            result=n1%n2;
+            break;  
+        case 'num3':
+            result=result+'3';
+            break; 
+                      
+    }
+    // connects html to java script operation
+    document.getElementById('result').value=result;
+}
